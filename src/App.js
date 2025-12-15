aead78d89922d88b957e1500ab0477bf848f5115
9345623562 <snehag5602@gmail.com>
2025-12-15 13:18:46 -0800

    code printed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
-import React from "react";
-class App extends React.Component
+function App()
 {
-  render()
-   {
-      return(<>
-       <h2>Welcome to React</h2>
-       <h2>Hello</h2>
-       <h2>Welcome to our Marriage</h2>
-       <h2>Manoj Weds Sneha</h2>
-       <h2>Invite You All</h2>
-       <p>Amet duis veniam exercitation aliquip eiusmod. Irure elit ex esse sunt proident sunt in. Laboris excepteur ea anim anim laborum. Adipisicing id consequat do ad consequat dolor Lorem. Labore aliqua officia ullamco cillum nisi tempor sunt ipsum cupidatat.
-
-Laborum aute ad magna nostrud amet reprehenderit enim voluptate aliquip esse nisi. Consectetur ullamco ut in consequat magna adipisicing sunt cupidatat commodo. Ullamco irure pariatur nisi velit esse elit. Esse velit elit aute nostrud nostrud mollit sit in aliquip Lorem id tempor reprehenderit dolore.-</p>
-       </>)
-       }
+  if(true)
+  {
+  var v=100
+  let l=200
+  const c=300
+  v++
+  l++
+  console.log("Let:"+l);
+  //c++
+  console.log("Let:"+c);
+  }
+    return(
+    <>
+      <h1>variable : global : (v)</h1>
+      <h1>does not work let and const : beacause local</h1>
+    </>
+  )
 }
 export default App
-/*import logo from './logo.svg';
+/*function App()
+{
+  let myfun=(a,b,c)=>{
+    var total=a+b+c
+    document.getElementById("res").innerHTML="<h2>Total:</h2>"+total;
+  }
+  return(
+    <>
+    <h2>Arrow Function with Parameter</h2>
+    <h4>Note:onClick c must be in caps<br></br>Don't use doublt qutoes</h4>
+    <h4>Note:in calling function don't use parenthesis when without passing args</h4>
+    <button onClick={()=>myfun(10,30,56)}>click me</button>
+    <div id="res"></div>
+    </>
+  )
+}
+export default App
+/*import myimg from './images/images.jpg'
+function App(){
+  return(
+    <>
+    <h2>Image Demo using path as expression</h2>
+    <center><img src={myimg} width="20%" height="auto" alt="path is wrong"></img></center>
+    </>
+  )
+}
+export default App
+/*function App(){
+  var a=250
+  var b=40
+  var c=300
+  return(
+    <>
+    <h2>Largest Amoung 3 Numbers</h2>
+    <h2>Largest Number:{a>b?(a>c?a:c):(b>c?b:c)}</h2>
+    </>
+  )
+}
+export default App
+function App(){
+  //Javascript learned code
+  var a=10
+  var b=30
+  var c="Sneha"
+
+  return(<>
+       <h1>Total:{a+b}</h1>
+       <marquee bgcolor='blue'><font size='7'>{c}</font></marquee>
+  </>)
+}
+export default App
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
